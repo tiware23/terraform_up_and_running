d2df06dcdb9f7c0cf9623a1339b48c42a99a43c9
Thiago Cavalcante <u003688@XPNOTE-20000329.xpcorretora.com.br>
2021-06-18 15:59:10 -0300

    Initial commit

--- aws/ch02/main.tf
+++ aws/ch02/main.tf
@@ -1,12 +1,75 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0aeeebd8d2ab47354"
-    instance_type = "t2.micro"
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 
-    tags = {
-        Name = "terraform-example"
-    }
+# resource "aws_instance" "example" {
+#   ami           = "ami-0aeeebd8d2ab47354"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.example.id] # implicity dependency
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p ${var.server_port} &
+#               EOF   
+
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0aeeebd8d2ab47354"
+  instance_type   = "micro"
+  security_groups = [aws_security_group.example.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF 
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "example" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
